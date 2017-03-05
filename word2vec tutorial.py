491058683c1557f42b1dde4fcbcc7f28a3af333f
Lesa Wright <lswright@gmail.com>
2017-03-05 21:16:54 +0000

    added tokenization progress
    needs to be reduced to just one line of XX% progress so far rather than
    the current repeated output.  I gues this would'nt be a problem in a
    GUI.

--- word2vec tutorial.py
+++ word2vec tutorial.py
@@ -1,4 +1,13 @@
 import pandas as pd
+#Import various modules for string cleaning
+from bs4 import BeautifulSoup
+import re
+from nltk.corpus import stopwords
+#Download the punkt tokenizer for sentence splitting
+import nltk.data
+#nltk.download()
+
+
 #Read data from files
 train = pd.read_csv("labeledTrainData.tsv",header=0,delimiter="\t",quoting=3)
 test = pd.read_csv("testData.tsv",header=0,delimiter="\t",quoting=3)
@@ -7,12 +16,9 @@ unlabeled_train = pd.read_csv("unlabeledTrainData.tsv",header=0,delimiter="\t",q
 print ("Read %d labeled train reviews, %d labeled test reviews," \
        "and %d unlabeled reviews\n" % (train['review'].size, test['review'].size,unlabeled_train['review'].size))
 
-#Import various modules for string cleaning
-from bs4 import BeautifulSoup
-import re
-from nltk.corpus import stopwords
 
-def review_to_wordlist(review, remove_stopwords=True):
+
+def review_to_wordlist(review, remove_stopwords=False):
     #Function to convert a dcoument to a sequence of words,
     #optionally removing stopwords
     #
@@ -32,7 +38,62 @@ def review_to_wordlist(review, remove_stopwords=True):
     #5. Return a list of words
     return(words)
 
-print(train.loc[0]['review'])
-print(review_to_wordlist(train.loc[0]['review']))
+#print(train.loc[0]['review'])
+#print(review_to_wordlist(train.loc[0]['review']))
+
+#Load the punkt tokenizer
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')        
+
+#Define a function to split a review into parsed sentences
+def review_to_sentences(review,tokenizer,remove_stopwords=False):
+    ## Function to split a review into parsed sentences. Returns a 
+    # list of sentences, where each sentence is a list of words
+    #This is the format that word2vec requires - a list (sentences) of lists (words)
+    
+    # 1. Use the NLTK tokenizer to split the paragraph into sentences
+    raw_sentences = tokenizer.tokenize(review.strip())
+    
+    # 2. Loop over each sentence
+    sentences =[]
+    for raw_sentence in raw_sentences:
+        # If a sentence is empty, skip it
+        if len(raw_sentence)>0:
+            #Call review_to_wordlist to get a list of words
+            sentences.append(review_to_wordlist(raw_sentence, remove_stopwords))
+    # Return the list of sentences - each sentence is a list of words, so this returns a list of lists        
+    return sentences
+
+#Now we can apply this function to prepare our data for input to Word2Vec (this will take a couple minutes):
+sentences = []
+
+
+print("Parsing sentences from Trained set")
+for review in range(train["review"].size):
+    #print("This is review ", review)
+    if review%100==0:
+        print(" %d percent progress so far" % (review/train["review"].size*100))
+    sentences += review_to_sentences(train["review"][review], tokenizer)
+
+print("Parsing sentences from Unlabeled set")
+for review in range(unlabeled_train["review"].size):
+    if review%100==0:
+        print(" %d percent progress so far" % (review/unlabeled_train["review"].size*100))
+    sentences += review_to_sentences(unlabeled_train["review"][review], tokenizer)
 
         
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
